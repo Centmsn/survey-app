f5e03036bb79f846e4a0ad1342e14d76bba059da
Wojtek Rygorowicz <wojciech.rygorowicz@gmail.com>
2021-04-20 11:10:53 +0200

    add billing route

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,14 +1,20 @@
+import { useDispatch } from "react-redux";
 import StripeCheckout from "react-stripe-checkout";
+import { handleToken } from "../actions/index";
 
 const Payments = () => {
+  const dispatch = useDispatch();
+
   return (
     <StripeCheckout
       amount={500}
-      token={token => console.log(token)}
+      token={token => dispatch(handleToken(token))}
       stripeKey={process.env.REACT_APP_STRIPE_KEY}
       name="SurveyApp"
       description="5$ for 5 email credits"
-    />
+    >
+      <button className="btn">Add Credits</button>
+    </StripeCheckout>
   );
 };
 
